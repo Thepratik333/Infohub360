ed12c63ee451e14e5224808034a8c6b7bd2296c3
Thepratik333 <110605777+Thepratik333@users.noreply.github.com>
2024-03-05 17:25:39 +0530

    Update Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -15,7 +15,7 @@ const Signup = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await fetch("https://infohub360.onrender.com/api/auth/getuser", {
+        const response = await fetch("http://localhost:5000/api/auth/getuser", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"

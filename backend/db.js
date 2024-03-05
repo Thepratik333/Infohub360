bbfe51155cee3b4c35af5eb0b6beacd084e20add
Thepratik333 <110605777+Thepratik333@users.noreply.github.com>
2024-03-05 16:29:58 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = "mongodb://localhost:27017/pratik";
+const mongoUri = "mongodb+srv://pratik:95j9EK8q9txmQSBu@cluster0.hfq2sgw.mongodb.net/pratik";
 
 const connectmongo = () => {
   mongoose.connect(mongoUri,{

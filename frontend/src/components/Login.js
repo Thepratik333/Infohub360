5551f001d0756b1d0cf05d0d78337714ce3056bd
Thepratik333 <110605777+Thepratik333@users.noreply.github.com>
2024-03-05 17:08:58 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     let navigate = useNavigate()
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("https://infohub360.onrender.com/api/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
